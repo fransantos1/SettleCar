7554793866047a1111cc387e0abb6b1948fc139f
Francisco Santos <49843015+fransantos1@users.noreply.github.com>
2023-05-02 11:42:31 +0100

    passar para o portatil

--- models/rentModel.js
+++ models/rentModel.js
@@ -14,7 +14,27 @@ class rent{
         this.usr_id = usr_id;
         this.rent_state = rent_state;
     }
-    static async createRent(car, usr_id){
+    static async createRent(start_date, return_date, car, usr_id){
+        try{
+            //Verify if the car is available
+            //Verify if the user has any other rent
+            let dbResult = await pool.query("SELECT * FROM rent WHERE rent_usr_id =$1 and rent_rentstate_id != 3", [usr_id]);
+            let Result = dbResult.rows;
+            if(Result.length){
+                return {
+                    status: 400, result: [{
+                        location: "body", param: "rent",
+                        msg: "This user already has a rent!"
+                    }]
+                }
+            }
+            let result = await pool.query("INSERT INTO rent(rent_data_inicio, rent_data_final, rent_car_id, rent_usr_id, rent_rentstate_id) VALUES ($1,$2,$3,$4,$5);",
+            [start_date, return_date,car.id,usr_id, 1]);
+
+            return { status: 200} ;
+        }catch(err){
+            console.log(err);
+        }
     
     }
 
